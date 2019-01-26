a30adefe5ba93cdbc4a436f5562536d9bbb34f9e
YOUR_USERNAMEisaaccho <cho_isaac@hotmail.com>
2019-01-26 14:43:46 -0500

    added profile component

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Profile.css'
+
+const Profile = () => {
+    return(
+        <div>
+            <p>abcd</p>
+
+        </div>
+    )
+}
+
+export default Profile
