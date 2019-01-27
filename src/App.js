1af2c2d430671d692733f89914c753a82036026d
YOUR_USERNAMEisaaccho <cho_isaac@hotmail.com>
2019-01-26 19:21:09 -0500

    update loading page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import profilePic from './profilePic.svg';
 import Profile from './Components/Profile/Profile';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
+
+
       <div className="App">
         <Profile />
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Test <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={profilePic} />
+
+
         </header>
       </div>
     );
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './Profile.css'
+import { Button, NavBar } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/lib/Navbar';
+
 
 const Profile = () => {
-    return(
+    return (
         <div>
-            <p>abcd</p>
-
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#home">Isaac Cho</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Text>
+                        | Points: <Navbar.Link href="https://www.facebook.com">1500 |</Navbar.Link>
+                    </Navbar.Text>
+                    <Navbar.Text pullLeft>Basketball</Navbar.Text>
+                    <Navbar.Text pullLeft>Jogging</Navbar.Text>
+                    <Navbar.Text pullLeft>Swimming</Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
+
     )
 }
 
