7fd50d25ab8679e028262b12135b3812a9d42f6f
YOUR_USERNAMEisaaccho <cho_isaac@hotmail.com>
2019-01-29 21:21:12 -0500

    edits to events section

--- src/Components/NearMe.js
+++ src/Components/NearMe.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { Button, NavBar, Image, StyleSheet} from 'react-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import './Events.css';
-import sampleimage from './LPField.jpg';
-
+import React from "react";
+import { Button, NavBar, Image, StyleSheet } from "react-bootstrap";
+import Navbar from "react-bootstrap/lib/Navbar";
+import "./Events.css";
+import sampleimage from "./LPField.jpg";
 
 const NearMe = () => {
-    return (
-        <div>
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="/home">Isaac Cho</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Text>
-                        | Points: <Navbar.Link href="/Rewards">1500 |</Navbar.Link>
-                    </Navbar.Text>
-                    <Navbar.Text pullLeft>Basketball</Navbar.Text>
-                    <Navbar.Text pullLeft>Jogging</Navbar.Text>
-                    <Navbar.Text pullLeft>Swimming</Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-            <div class="header" >Events Near Me</div>
-            <div class="card">
-            <h1>Soccer at LesPrince Field</h1>
-            <h2>Dec 7, 2017</h2>
-            <img src={sampleimage} />
-            <p>Description of event blahblah</p>
-            </div>
-        </div>
+  return (
+    <div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/home">Isaac Cho</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Text>
+            | Points: <Navbar.Link href="/Rewards">1500 |</Navbar.Link>
+          </Navbar.Text>
+          <Navbar.Text pullLeft>Basketball</Navbar.Text>
+          <Navbar.Text pullLeft>Jogging</Navbar.Text>
+          <Navbar.Text pullLeft>Swimming</Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+      <div class="header">Events Near Me</div>
+      <div class="card">
+        <h1>Soccer at LesPrince Field</h1>
+        <h2>Dec 7, 2017</h2>
+        <img src={sampleimage} />
+        <p>
+          The picture shown is the place of meetup, please show up before 6pm
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default NearMe
+export default NearMe;
