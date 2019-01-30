d60e4db7fd5d913732b2f283665ea91da82022a9
YOUR_USERNAMEisaaccho <cho_isaac@hotmail.com>
2019-01-29 21:17:02 -0500

    changes to profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import styles from './Profile.css'
-import { Button, NavBar } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
-
-
+import React from "react";
+import styles from "./Profile.css";
+import { Button, NavBar } from "react-bootstrap";
+import Navbar from "react-bootstrap/lib/Navbar";
 
 const Profile = () => {
-    return (
-        <div>
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#home">Isaac Cho</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Text>
-                        | Points: <Navbar.Link href="/Rewards">1500 |</Navbar.Link>
-                    </Navbar.Text>
-                    <Navbar.Text pullLeft>Basketball</Navbar.Text>
-                    <Navbar.Text pullLeft>Jogging</Navbar.Text>
-                    <Navbar.Text pullLeft>Swimming</Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
+  return (
+    <div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Isaac Cho</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Text>
+            | Points: <Navbar.Link href="/Rewards">1500 |</Navbar.Link>
+          </Navbar.Text>
+          <Navbar.Text pullLeft>Basketball</Navbar.Text>
+          <Navbar.Text pullLeft>Jogging</Navbar.Text>
+          <Navbar.Text pullLeft>Swimming</Navbar.Text>
+          <Navbar.Text pullRight>Status: Active</Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Profile
+export default Profile;
